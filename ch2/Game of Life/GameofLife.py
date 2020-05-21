c5b12210174c8bf436181e83fc7f52563757c5c9
Predstan <58338603+Predstan@users.noreply.github.com>
2020-05-21 05:10:36 -0500

    Add files via upload

--- ch2/Game of Life/GameofLife.py
+++ ch2/Game of Life/GameofLife.py
@@ -1,39 +1,55 @@
+# Implements the Game of Life Program
 from LifeGrid import LifeGrid
 
+# Configuration for the Live Organisms
 init_cofig = [(1,1), (1, 2), (2, 2), (3, 2)]
 
+# Number of Columns and Rows on Grid
 grid_width = 5
 grid_height = 5
 
+# Number of generation
 num_gen = 8
 
+
 def main():
-    
+    # Constructs the Main Grid and Configures it
     grid = LifeGrid(grid_width, grid_height)
     grid.configure(init_cofig)
+    
+    # Play Game
+    print("Start Point")
     draw(grid)
     for i in range(num_gen):
         evolve(grid)
         print("\n")
+        print("Generation", i+1)
         draw(grid)
 
+# Function for regenerating live and dead organisms according to the Rules 
 def evolve(grid):
-
+    # Creates a list for live cells after each generation
     live_cells = list()
 
+    # Iterates over the grid for regeneration of cells according to the rule
     for i in range(grid.numRows()):
         for j in range(grid.numCols()):
 
+            # Determine Number of live Neigbors
             neighbours = grid.numLiveNeighbnors(i, j)
 
+            # Regenetion from live and dead cells
             if (neighbours == 2 and grid.isLiveCell(i, j)) or\
                 (neighbours == 3):
                 live_cells.append((i, j))
 
+    # Reconfigure live cells
     grid.configure(live_cells)
 
+# Draw Grid
 def draw(grid):
     for row in range(grid.numRows()):
             print("  ".join(grid.grid.numRow[row]))
 
+# Execute Main Function
 main()
--- ch2/Game of Life/LifeGrid.py
+++ ch2/Game of Life/LifeGrid.py
@@ -1,21 +1,26 @@
+# Implementing the LIFE GRID ADT for Game of Life
+
 from Array2D import Array2D
 
 class LifeGrid:
+    # Defines the Constant to reperesent the Live and Dead Cells
     live = "@"
     dead = "."
+
+    # Creating Game Grids
     def __init__(self, numRow, numCol):
         self.grid = Array2D(numRow, numCol)
         self.configure(list())
 
-
+    # Returns the Number of Rows of the Grid
     def numRows(self):
         return self.grid.numRows()
 
-
+    # Returns the Number of Columns of the Grid
     def numCols(self):
         return self.grid.numCols()
 
-
+    # Clears and Configures the Live cell 
     def configure(self, coordlist):
         for i in range(self.numRows()):
             for j in range(self.numCols()):
@@ -25,31 +30,29 @@ class LifeGrid:
         for cell in coordlist:
             self.setCell(cell[0], cell[1])
 
-
+    # Set Cell to dead Cell 
     def clearCell(self, row, col):
         assert row >= 0 and row < self.grid.numRows()\
            and col >= 0 and col < self.grid.numCols(),\
                 "Invalid Range in Grid"
         self.grid[row, col] = LifeGrid.dead
 
-
+    # Set cell to live cell
     def setCell(self, row, col):
         assert row >= 0 and row < self.grid.numRows()\
            and col >= 0 and col < self.grid.numCols(),\
                 "Invalid Range in Grid"
         self.grid[row, col] = LifeGrid.live
 
-
+    # Returns True if Cell is Live
     def isLiveCell(self, row, col):
         assert row >= 0 and row < self.grid.numRows()\
            and col >= 0 and col < self.grid.numCols(),\
                 "Invalid Range in Grid"
         if self.grid[row, col] == LifeGrid.live:
             return True
-        else:
-            return False
 
-
+    # Retuen number of neighbor's Cells
     def numLiveNeighbnors(self, row, col):
         assert row >= 0 and row < self.grid.numRows()\
            and col >= 0 and col < self.grid.numCols(),\
