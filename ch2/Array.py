f7fbe59b6356a54ec2ff0368a592396ccf3c8345
Predstan <58338603+Predstan@users.noreply.github.com>
2020-05-22 00:47:33 -0500

    Add files via upload

--- ch2/Array.py
+++ ch2/Array.py
@@ -2,7 +2,7 @@
 import ctypes
 
 class Array:
-    # Creates the Array with the givem size of the Array
+    # Creates the Array with the given size of the Array
     def __init__(self, size):
         assert size > 0, 'Array Must be greater than 0'
         self.size = size
@@ -10,7 +10,7 @@ class Array:
         ArrayTypes = ctypes.py_object * size
         self.slots = ArrayTypes()
 
-        self.clear(0)
+        self.clear(None)
 
     # Returns the size of the Array
     def __len__(self):
@@ -30,25 +30,30 @@ class Array:
     def clear(self, value):
         for i in range(self.size):
             self.slots[i] = value
-    # Iterating over the array
+    
+    # Returns Iteratable list
     def __iter__(self):
         return Arrayiterator(self.slots)
 
 
 
 class Arrayiterator:
-    # Creates 
+    # Creates an object of iteration
+    idx = 0
+
+    # Creates the iteratable List
     def __init__(self, thelist):
         self.list = thelist
-        self.idx = 0
-
+        
+    # Returns Iteratable Array
     def __iter__(self):
-        return self
+        return self.list
 
+    # Returns next occuring Value
     def __next__(self):
-        if self.idx <len(self.list):
-            ent = self.list[self.idx]
-            self.idx+=1
-            return ent
+        if Arrayiterator.idx  < len(self.list):
+           let = self.list[Arrayiterator.idx ]
+           Arrayiterator.idx  += 1
+           return let
         else:
             raise StopIteration
