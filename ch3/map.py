4fcc3f7f93cdfb13bdbcfc327a2832b20583c2f5
Predstan <58338603+Predstan@users.noreply.github.com>
2020-05-27 04:03:20 -0500

    Add files via upload

---
+++ ch3/map.py
@@ -0,0 +1,110 @@
+from Array import Array
+# Implementation of Mapping ADT using a single list
+
+class Map:
+    # Creates an Instance of an empty Map
+    def __init__(self):
+        self.map = list()
+
+    # Returns the Number of key in the Map
+    def __len__(self):
+        return len(self.map)
+
+    # Returns True if key is contained in the Map
+    def __contains__(self, key):
+        ndx = self.findPosition( key )
+        return ndx is not None
+
+    # Adds new key and Add Value if key not already in the Map
+    # Otherwise, Adds replaces the value of the key existing
+    def add(self, key, value):
+        ndx = self.findPosition( key )
+        if ndx is not None: # If Key is in Map
+            self.map[ndx].value = value # Replaces Value
+            return False
+        else: # Otherwise key not in Map
+            entry = MapEntry(key, value) 
+            self.map.append(entry)
+            return True
+
+    # Returns the Value of Key if Key is in the Map
+    def valueOf(self, key): 
+        ndx = self.findPosition(key)
+        if ndx is not None:
+            return self.map[ndx].value
+        
+        return "Invalid Key"
+
+    # Removes Key in map is key is present in Map
+    def remove(self, key):
+        ndx = self.findPosition( key )
+        assert ndx is not None, "Invalid Key"
+        self.map.remove(key)
+
+    # Iterates over Keys in the Map
+    def __iter__(self):
+        return mapIterator(self.map)
+
+    # Returns the Position of a key in the Map,
+    # Otherwise, returms None
+    def findPosition(self, key):
+        for i in range(len(self.map)):
+            if self.map[i] == key:
+                return i
+        return None
+
+    # Sets the value of a particular key
+    def __setitem__(self, key, value):
+        ndx = self.findPosition( key )
+        if ndx is not None: # If Key is in Map
+            self.map[ndx].value = value # Replaces Value
+            return False
+        else: # Otherwise key not in Map
+            entry = MapEntry(key, value) 
+            self.map.append(entry)
+            return True
+
+    # Returns the value of a key
+    def __getitem__(self, key):
+        ndx = self.findPosition(key)
+        if ndx is not None:
+            return self.map[ndx].value
+        
+        return "Invalid Key"
+
+
+    def keyArray(self):
+        keyArray = Array(len(self))
+        for i in range(len(self)):
+            keyArray[i] = self[i]
+
+
+# Class for Key Value parts
+class MapEntry:
+
+    def __init__(self, key, value):
+        self.value = value
+        self.key = key
+ 
+
+class mapIterator(Map):
+
+    def __init__(self, thelist):
+        self.thelist = thelist
+        self.ndx = 0
+
+
+    def __iter__(self):
+        return self.thelist
+
+
+    def __next__(self):
+        if len(self.ndx) < len(self.thelist):
+            key = self.thelist[self.ndx]
+            self.ndx += 1
+            return key
+
+        raise StopIteration
+
+
+
---
+++ ch3/set.py
@@ -0,0 +1,148 @@
+# Implementation of Set ADT using Python List
+
+class Set:
+
+    # Creates an Empty Set instance
+    def __init__(self, *initialElements):
+        
+        self.elements = list(initialElements)
+    
+
+    # Returns the Length of elements in the set
+    def __len__(self):
+        return len(self.elements)
+
+    # Returns True if element is contained in the set
+    def __contains__(self, element):
+        return element in self.elements
+
+    # Adds Elements only when Element is not already in the set
+    def add(self, element):
+        if element not in self.elements:
+            self.elements.append(element)
+
+    # Removes the element if already in the list 
+    def remove(self, element):
+        assert element in self, "The element not in Set"
+        self.elements.remove(element)
+
+    # Returns True if two sets are equal
+    def __eq__(self, setB):
+        if len(self) != len(setB):
+            return False
+        else:
+            return self.isSubsetOf(setB)
+
+    # Returns True if set A is a subset of Set B
+    def isSubsetOf(self, setB):
+        for element in self:
+            if element not in setB:
+                return False
+        return True
+
+    # Returns True if Set is a proper Set
+    def properSet(self, setB):
+        return self.isSubsetOf(setB) and self != setB
+
+    
+    def __str__(self):
+       return '{%s}' % str(self.elements).strip("[]")
+
+
+    # Returns the Union of Set A and Set B as a new set
+    def union(self, setB):
+        newSet = Set()
+        newSet.elements.extend(self)
+        for element in setB:
+            if element not in self:
+                newSet.elements.append(element)
+        return newSet
+
+    # Returns the Intersect of Set A and Set B  as a new set 
+    def intersect(self, setB):
+        newSet = Set()
+        for element in self:
+            if element in setB:
+                newSet.add(element)
+        return newSet
+
+    # Returns the difference unique elements of Set A and Set B as a new set
+    def difference(self, setB):
+        newSet = Set()
+        for element in self:
+            if element not in setB:
+                newSet.add(element)
+        return newSet
+
+    # Returns the Union of 2 Sets using python operator add
+    def __add__(self, setB):
+        newSet = Set()
+        newSet.elements.extend(self)
+        for element in setB:
+            if element not in self:
+                newSet.elements.append(element)
+        return newSet
+
+     # Returns the intersect of 2 Sets using python operator mul
+    def __mul__(self, setB):
+        newSet = Set()
+        for element in self:
+            if element in setB:
+                newSet.add(element)
+        return newSet
+
+    # Returns the difference of 2 Sets using python operator sub
+    def __sub__(self, setB):
+        newSet = Set()
+        for element in self:
+            if element not in setB:
+                newSet.add(element)
+        return newSet
+
+    # Determines if a Set is a Subset of another Set B
+    def __lt__(self, setB):
+        for element in self:
+            if element not in setB:
+                return False
+        return True
+
+
+
+
+
+
+    # Iterates over the set
+    def __iter__(self):
+        return SetIterator(self.elements)
+
+
+
+class SetIterator(Set):
+
+    # Creates an instance of the list
+    def __init__(self, elements):
+        self.elements = elements
+        self.ndx = 0
+
+    # Returns the Iteration 
+    def __iter__(self):
+        return self.elements
+
+    # Returns the Next Element
+    def __next__(self):
+        if self.ndx < len(self.elements):
+            new = self.elements[self.ndx]
+            self.ndx += 1
+            return new
+        
+        raise StopIteration
+
+
+
+get = Set(8, 9, 1, 6, 100)
+hide = Set(8, 9, 1, 6, 7, 2, 3, 4)
+print(len(hide))
+print(get.properSet(hide))
+print(get.__str__())
+    
+    
