cca8adf0dd5f414337378097f9ef674049c99547
Predstan <58338603+Predstan@users.noreply.github.com>
2020-06-09 07:19:25 -0500

    Add files via upload

--- ch6/LinkedList.py
+++ ch6/LinkedList.py
@@ -31,13 +31,11 @@ def deleteNode(head, target):
 
     if curNode is not None:
         if target == head.data:
-            print(curNode.next.data)
             head = curNode.next
 
         else:
             prenode.next = curNode.next
     return head
-
 
 
 head = LinkedList("a")
@@ -50,22 +48,112 @@ head = NewNode
 transversal(head)
 print(unorderedSearch(head, 4))
 print("\n")
-deleteNode(head, 4)
-transversal(head)
-print("\n")
-prenode = None
-curNode = head
-while curNode is not None and curNode.data != 4:
-    prenode = curNode
-    curNode = curNode.next
-if curNode is not None:
-    if 4 == head.data:
-        head = curNode.next
-        print(head.data)
+transversal(deleteNode(head, "a"))
 
+# Add a Node when Head and Tail is referenced
+def add(head, tail, target):
+    NewNode = LinkedList( target )
+    if head is None:
+        head = NewNode
     else:
-        prenode.next = curNode.next
-transversal(head)
+        tail.next = NewNode
+    tail = NewNode
 
+# Add with only head referenced
+def addUnsorted(head, target):
+    NewNode = LinkedList(target)
+    if head is None:
+        NewNode = head
+    else:
+        NewNode.next = head
+        head = NewNode
 
-print(1 == 1.0)
+# Remove a Node when Tail and Head is Given
+def remove(head, tail, target):
+    prenode = None
+    curNode = head
+    while curNode is not None and curNode.data != target:
+        prenode = curNode
+        curNode = curNode.next
+
+    if curNode is not None:
+        if curNode == head:
+            head = curNode.next
+        if curNode == tail:
+            tail = prenode
+        else:
+            prenode.next = curNode.next
+
+# Search Sorted LinkedList
+def SearchSorted(head, target):
+    curNode = head
+    while curNode is not None and curNode.data < target:
+        if curNode.data == target:
+            return True
+
+        else:
+            curNode = curNode.next
+    return False
+# Add in a Sorted Linked List
+def addSorted(head, target):
+    preNode = None
+    curNode = head
+    while curNode is not None and curNode.data < target:
+        preNode = curNode
+        curNode = curNode.next
+    
+    NewNode = LinkedList( target )
+    NewNode.next = curNode
+    if curNode is head:
+        head = NewNode
+    else:
+        preNode.next = NewNode
+
+    return head
+
+def deleteSorted(head, target):
+    preNode = None
+    curNode = head
+
+    while curNode is not None and curNode.data < target:
+        preNode = curNode
+        curNode = curNode.next
+
+    if curNode is not None:
+        if head.data == target:
+            head = curNode.next
+        elif curNode.data == target:
+            preNode.next = curNode.next
+
+def removeall(head):
+    curNode = head
+    preNode = None
+    while curNode is not None:
+        preNode = curNode.next
+        curNode = preNode
+
+    return curNode
+
+def splitInHalf(head):
+    n = length(head)
+    curNode = head
+    n = n//2
+    m = 0
+    while m != n :
+        m += 1
+        curNode = curNode.next
+
+    return curNode
+
+def length(head):
+    curNode = head
+    n = 0
+    if curNode is None:
+        return None 
+
+    while curNode is not None:
+        n +=1
+        curNode = curNode.next
+
+    return n
+    
