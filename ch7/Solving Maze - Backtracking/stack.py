f6c47dbb08b154a52b50753f54ff92b14bdb2349
Predstan <58338603+Predstan@users.noreply.github.com>
2020-06-15 02:27:47 -0500

    Add files via upload

---
+++ ch7/Solving Maze - Backtracking/stack.py
@@ -0,0 +1,39 @@
+# Linked List Implementation of Stack ADT
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    # determines if the Stack is empty
+    def isEmpty(self):
+        return self.head == None
+
+    # Returns the number of elements in the Stack
+    def length(self):
+        return self.size
+
+    # Removes and returns the Last in element
+    def pop(self):
+        assert self.isEmpty() is not True,\
+            "Stack is empty"
+        item = self.head.item
+        self.head = self.head.next
+        self.size -= 1
+        return item
+
+    # Check the Last element into the Stack
+    def peek(self):
+        assert self.isEmpty() is not False,\
+            "Stack is empty"
+        return self.head.item
+
+    # Push an element into the Stack
+    def push(self, item):
+        self.head = StackNode( item, self.head )
+        self.size += 1
+
+class StackNode(object):
+
+    def __init__(self, item, base):
+        self.item = item
+        self.next = base
